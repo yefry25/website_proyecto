9cb613d58e118110b99fc3b36e6160517d5e3bf1
AIO-025\Jefferson Becerra <jefferson.becerra@tecnofactory.com.co>
2025-09-04 09:36:26 -0500

    ajustes en rutas y archivo angular.json

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { VideoGameComponent } from './features/video-game/video-game.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'video-game', component: VideoGameComponent }
+];
