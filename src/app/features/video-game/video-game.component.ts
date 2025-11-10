a6514c13521804b55ed0e7abbdd6c0c4098c8062
AIO-025\Jefferson Becerra <jefferson.becerra@tecnofactory.com.co>
2025-11-10 10:45:16 -0500

    se terminan detalles de visualización y mejoras en la lógica del juego

--- src/app/features/video-game/video-game.component.ts
+++ src/app/features/video-game/video-game.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { trigger, state, style, transition, animate, keyframes, AnimationEvent } from '@angular/animations';
+import Swal from 'sweetalert2';
 
 interface TrashItem {
   id: number;
-  type: 'organic' | 'plastic' | 'paper';
+  type: 'organic' | 'recyclable-waste' | 'non-recyclable-waste';
   img: string;
   name: string;
   visible: boolean;
@@ -62,51 +63,37 @@ export class VideoGameComponent {
 
   public isGameActive = false;
   public score = 0;
-  public timeLeft = 60;
+  public timeLeft = 70;
   private timerInterval: any;
   public draggedItem: TrashItem | null = null;
 
-  // public trashItems: TrashItem[] = [
-  //   { id: 1, type: 'organic', icon: '🍎', name: 'Manzana', visible: true, state: 'default' },
-  //   { id: 2, type: 'plastic', icon: '🧴', name: 'Botella', visible: true, state: 'default' },
-  //   { id: 3, type: 'paper', icon: '📰', name: 'Periódico', visible: true, state: 'default' },
-  //   { id: 4, type: 'organic', icon: '🍌', name: 'Plátano', visible: true, state: 'default' },
-  //   { id: 5, type: 'plastic', icon: '🥤', name: 'Vaso', visible: true, state: 'default' },
-  //   { id: 6, type: 'paper', icon: '📦', name: 'Caja', visible: true, state: 'default' },
-  // ];
-
   public trashItems: TrashItem[] = [
-    // 🟢 Orgánicos
+    { id: 12, type: 'recyclable-waste', img: 'assets/trash/soda-cap.jpg', name: 'Tapa de gaseosa', visible: true, state: 'default' },
     { id: 1, type: 'organic', img: 'assets/trash/apple-core.jpg', name: 'Cáscara de manzana', visible: true, state: 'default' },
-    { id: 2, type: 'organic', img: 'assets/trash/banana-peel.jpg', name: 'Cáscara de plátano', visible: true, state: 'default' },
-    { id: 3, type: 'organic', img: 'assets/trash/eggshells.jpg', name: 'Cáscaras de huevo', visible: true, state: 'default' },
-    { id: 4, type: 'organic', img: 'assets/trash/vegetable-peels.jpg', name: 'Cáscaras de vegetales', visible: true, state: 'default' },
+    { id: 7, type: 'recyclable-waste', img: 'assets/trash/plastic-bottle.jpg', name: 'Botella plástica', visible: true, state: 'default' },
+    { id: 9, type: 'recyclable-waste', img: 'assets/trash/straw.jpg', name: 'Pitillo', visible: true, state: 'default' },
     { id: 5, type: 'organic', img: 'assets/trash/coffee-grounds.jpg', name: 'Restos de café', visible: true, state: 'default' },
+    { id: 10, type: 'recyclable-waste', img: 'assets/trash/yogurt-cup.jpg', name: 'Vaso de yogur', visible: true, state: 'default' },
+    { id: 19, type: 'recyclable-waste', img: 'assets/trash/magazine.jpg', name: 'Revista', visible: true, state: 'default' },
+    { id: 4, type: 'organic', img: 'assets/trash/vegetable-peels.jpg', name: 'Cáscaras de vegetales', visible: true, state: 'default' },
+    { id: 15, type: 'non-recyclable-waste', img: 'assets/trash/tissue.jpg', name: 'Pañuelo usado', visible: true, state: 'default' },
+    { id: 13, type: 'recyclable-waste', img: 'assets/trash/newspaper.jpg', name: 'Periódico', visible: true, state: 'default' },
+    { id: 16, type: 'non-recyclable-waste', img: 'assets/trash/napkin.jpg', name: 'Servilleta sucia', visible: true, state: 'default' },
     { id: 6, type: 'organic', img: 'assets/trash/food-leftovers.jpg', name: 'Sobras de comida', visible: true, state: 'default' },
-
-    // ⚪ Plástico
-    { id: 7, type: 'plastic', img: 'assets/trash/plastic-bottle.jpg', name: 'Botella plástica', visible: true, state: 'default' },
-    { id: 8, type: 'plastic', img: 'assets/trash/plastic-bag.jpg', name: 'Bolsa plástica', visible: true, state: 'default' },
-    { id: 9, type: 'plastic', img: 'assets/trash/straw.jpg', name: 'Pitillo', visible: true, state: 'default' },
-    { id: 10, type: 'plastic', img: 'assets/trash/yogurt-cup.jpg', name: 'Vaso de yogur', visible: true, state: 'default' },
-    { id: 11, type: 'plastic', img: 'assets/trash/shampoo-bottle.jpg', name: 'Envase de champú', visible: true, state: 'default' },
-    { id: 12, type: 'plastic', img: 'assets/trash/soda-cap.jpg', name: 'Tapa de gaseosa', visible: true, state: 'default' },
-
-    // ⚫ Papel / no aprovechable
-    { id: 13, type: 'paper', img: 'assets/trash/newspaper.jpg', name: 'Periódico', visible: true, state: 'default' },
-    { id: 14, type: 'paper', img: 'assets/trash/cardboard.jpg', name: 'Cartón', visible: true, state: 'default' },
-    { id: 15, type: 'paper', img: 'assets/trash/tissue.jpg', name: 'Pañuelo usado', visible: true, state: 'default' },
-    { id: 16, type: 'paper', img: 'assets/trash/napkin.jpg', name: 'Servilleta sucia', visible: true, state: 'default' },
-    { id: 17, type: 'paper', img: 'assets/trash/paper-cup.jpg', name: 'Vaso de papel', visible: true, state: 'default' },
-    { id: 18, type: 'paper', img: 'assets/trash/pizza-box.jpg', name: 'Caja de pizza', visible: true, state: 'default' },
-    { id: 19, type: 'paper', img: 'assets/trash/magazine.jpg', name: 'Revista', visible: true, state: 'default' },
-    { id: 20, type: 'paper', img: 'assets/trash/broken-paper.jpg', name: 'Papel roto', visible: true, state: 'default' },
+    { id: 17, type: 'non-recyclable-waste', img: 'assets/trash/paper-cup.jpg', name: 'Vaso de papel', visible: true, state: 'default' },
+    { id: 14, type: 'recyclable-waste', img: 'assets/trash/cardboard.jpg', name: 'Cartón', visible: true, state: 'default' },
+    { id: 3, type: 'organic', img: 'assets/trash/eggshells.jpg', name: 'Cáscaras de huevo', visible: true, state: 'default' },
+    { id: 18, type: 'non-recyclable-waste', img: 'assets/trash/pizza-box.jpg', name: 'Caja de pizza', visible: true, state: 'default' },
+    { id: 8, type: 'recyclable-waste', img: 'assets/trash/plastic-bag.jpg', name: 'Bolsa plástica', visible: true, state: 'default' },
+    { id: 20, type: 'non-recyclable-waste', img: 'assets/trash/broken-paper.jpg', name: 'Papel roto', visible: true, state: 'default' },
+    { id: 2, type: 'organic', img: 'assets/trash/banana-peel.jpg', name: 'Cáscara de plátano', visible: true, state: 'default' },
+    { id: 11, type: 'recyclable-waste', img: 'assets/trash/shampoo-bottle.jpg', name: 'Envase de champú', visible: true, state: 'default' },
   ];
-  
-  public bins : any[] =[
-    {type: 'organic', label: 'residuos orgánicos aprovechables', img: 'assets/bins/green-bin.png'},
-    {type: 'plastic', label: 'residuos aprovechables', img: 'assets/bins/white-bin.png'},
-    {type: 'paper', label: 'residuos no aprovechables', img: 'assets/bins/black-bin.png'}
+
+  public bins: any[] = [
+    { type: 'organic', label: 'residuos orgánicos aprovechables', img: 'assets/bins/green-bin.png' },
+    { type: 'recyclable-waste', label: 'residuos aprovechables', img: 'assets/bins/white-bin.png' },
+    { type: 'non-recyclable-waste', label: 'residuos no aprovechables', img: 'assets/bins/black-bin.png' }
   ]
 
   ngOnDestroy() {
@@ -121,7 +108,7 @@ export class VideoGameComponent {
 
     this.isGameActive = true;
     this.score = 0;
-    this.timeLeft = 60;
+    this.timeLeft = 70;
 
     // Muestra todos los ítems
     this.trashItems.forEach(item => (item.visible = true));
@@ -161,7 +148,7 @@ export class VideoGameComponent {
   }
 
   // Drop
-  public onDrop(event: DragEvent, binType: 'organic' | 'plastic' | 'paper'): void {
+  public onDrop(event: DragEvent, binType: 'organic' | 'recyclable-waste' | 'non-recyclable-waste'): void {
     if (!this.isGameActive || !this.draggedItem) return;
     event.preventDefault();
 
@@ -174,6 +161,40 @@ export class VideoGameComponent {
       // Penalización por error
       this.draggedItem.state = 'incorrect';
       this.score = Math.max(0, this.score - 5);
+    }
+
+    if(this.score === 200){
+      Swal.fire({
+        title: '¡Felicidades!',
+        text: 'Has alcanzado la puntuación máxima de 200 puntos. ¡Eres un experto en clasificación de residuos!',
+        icon: 'success',
+        confirmButtonText: 'Cerrar',
+        confirmButtonColor: '#2a7236ff'
+      }).then(() => {
+        this.startGame();
+      });
+    }
+    else if(this.score === 0){
+      Swal.fire({
+        title: '¡Sigue intentando!',
+        text: 'Tu puntuación ha bajado a 0 puntos. ¡No te desanimes, puedes mejorar tus habilidades de clasificación de residuos!',
+        icon: 'info',
+        confirmButtonText: 'Cerrar',
+        confirmButtonColor: '#2a7236ff'
+      }).then(() => {
+        this.startGame();
+      });
+    }
+    else if(this.timeLeft === 0){
+      Swal.fire({
+        title: '¡Tiempo agotado!',
+        text: 'Se acabó el tiempo. ¡Intenta de nuevo!',
+        icon: 'error',
+        confirmButtonText: 'Cerrar',
+        confirmButtonColor: '#2a7236ff'
+      }).then(() => {
+        this.startGame();
+      });
     }
 
     this.draggedItem = null;
